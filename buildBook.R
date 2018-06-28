5335b8b2726de384c2547df6a76ac8d7b37c84bd
Adrian <ar.hordyk@gmail.com>
2018-06-28 15:53:31 -0700

    update manual

--- buildBook.R
+++ buildBook.R
@@ -6,7 +6,7 @@ unlink('_book', force=TRUE, recursive = TRUE) # delete old book
 
 suppressWarnings(file.remove("userguide.rmd")) # delete the old debug file if present
 
-rmarkdown::render_site(output_format = c('bookdown::gitbook', 'bookdown::pdf_book'), encoding = 'UTF-8')
+rmarkdown::render_site(output_format = c('bookdown::gitbook')) # , 'bookdown::pdf_book'), encoding = 'UTF-8')
 
 if(!dir.exists(file.path('_book', 'docs'))) dir.create(file.path('_book', 'docs'), recursive = TRUE)
 
